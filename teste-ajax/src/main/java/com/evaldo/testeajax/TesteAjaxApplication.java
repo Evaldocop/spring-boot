6ba5d6e817be7b64f40b221015ee47cb305b6e0f
Evaldo <evaldocop@yahoo.com.br>
2021-12-07 17:05:57 -0300

    teste

---
+++ teste-ajax/src/main/java/com/evaldo/testeajax/TesteAjaxApplication.java
@@ -0,0 +1,29 @@
+package com.evaldo.testeajax;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.evaldo.testeajax.domain.SocialMetaTag;
+import com.evaldo.testeajax.service.SocialMetaTagService;
+
+@SpringBootApplication
+public class TesteAjaxApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(TesteAjaxApplication.class, args);
+	}
+	
+	@Autowired
+	private SocialMetaTagService service;
+
+	@Override
+	public void run(String... args) throws Exception {
+//	    SocialMetaTag tag = service.getSocialMetaTagbyUrl("https://www.udemy.com/course/spring-boot-mvc-com-thymeleaf/");
+//		System.out.println(tag.toString());
+		
+	}
+
+}
