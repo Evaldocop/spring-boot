914f0323c855df4274daf8d283bf6f5ea9b04074
Evaldo <evaldocop@yahoo.com.br>
2021-12-21 20:51:38 -0300

    curtindo likes

--- teste-ajax/src/main/resources/static/js/promo-list.js
+++ teste-ajax/src/main/resources/static/js/promo-list.js
@@ -2,68 +2,86 @@
 var pageNumber = 0;
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 	$("#loader-img").hide();
 	$("#fim-btn").hide();
 
 });
 
-$(window).scroll( function(){
+$(window).scroll(function() {
 
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
 	//console.log('scrollTop : ',scrollTop,' conteudo: ',conteudo);
-	
+
 	//onde entra o ajax
-	if(scrollTop>=conteudo){
+	if (scrollTop >= conteudo) {
 		///console.log('ajax entra aqui');
-		
+
 		pageNumber++;
-		
-		setTimeout(function(){
-		
+
+		setTimeout(function() {
+
 			finByScollBar(pageNumber);
-		
+
 		}, 200);
 	}
 });
 
 
 
-function finByScollBar(pageNumber){
+function finByScollBar(pageNumber) {
 
-    $.ajax({
-    
-    	method:"GET",
-    	url: "/promocao/list/ajax",
-    	data : {
-    		page:pageNumber  	 
-    	},
-    	
-    	beforeSend: function() {
-    	 
-    	 	$("#loader-img").show();
-    	 
-    	},
-    	success: function(response){
-    	
-			  if(response.length > 150){  	
-			    		$(".row").fadeIn(250, function(){   	
-			    	       $(this).append(response);   	
-			    		});    
-			  }else{			  	
-	            $("#fim-btn").show();
-			    $("#loader-img").removeClass("loader");
-			  }	
-    	},  	
-    	error:function(xhr){
-      		alert("Ops, ocorreu um erro",xhr.status, xhr.statusText); 	
-    	},
-    	complete: function() { 	 
-    	 	$("#loader-img").hide();
-    	},
-    	
-    	
-     });
+	$.ajax({
+		method: "GET",
+		url: "/promocao/list/ajax",
+		data: {
+			page: pageNumber
+		},
+		beforeSend: function() {
+			$("#loader-img").show();
+		},
+		success: function(response) {
+			if (response.length > 150) {
+				$(".row").fadeIn(250, function() {
+					$(this).append(response);
+				});
+			} else {
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+		},
+		error: function(xhr) {
+			alert("Ops, ocorreu um erro", xhr.status, xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();
+		},
+	});
 }
+///likes na promoções
+
+$("button[id*='likes-btn-']").on("click", function() {
+	var id = $(this).attr("id").split("-")[2];
+	console.log("id:", id);
+	$.ajax({
+		method: "POST",
+		url: "/promocao/like/" + id,
+		success: function(response) {
+			$("#likes-count-" + id).text(response);
+		},
+		error: function(xhr) {
+			alert("ops,ocorreu um erro:" + xhr.status + ", " + xhr.statusText);
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
