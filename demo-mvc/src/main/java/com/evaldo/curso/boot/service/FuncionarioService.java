461f4430038bd5458b3985ff8f1aa0a3a06bce75
Evaldoevaldo@pm.ba.gov.br <Evaldoevaldo@pm.ba.gov.br>
2022-02-24 15:28:42 -0300

    add service

---
+++ demo-mvc/src/main/java/com/evaldo/curso/boot/service/FuncionarioService.java
@@ -0,0 +1,18 @@
+package com.evaldo.curso.boot.service;
+
+import java.util.List;
+
+import com.evaldo.curso.boot.domain.Funcionario;
+
+public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+}
